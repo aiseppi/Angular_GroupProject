61e68b706c1e76faa57db08e488b544c105d49bc
Alyssa Crechiolo <alyssacrechiolo@gmail.com>
2020-05-21 13:01:59 -0400

    html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { WatchlistPageComponent } from './watchlist-page/watchlist-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "movielist", component: MovieListComponent },
+  { path: "watchlist", component: WatchlistPageComponent },
+  { path: "", redirectTo: "/movielist", pathMatch: "full" },
+  { path: "**", redirectTo: "/movielist", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  apiUrl: string = "https://api.themoviedb.org/3/movie/550";
+  apiKey: string = "780adf32117a98ba4db8e7489d68fc1b";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getData(): any {
+    return this.http.get(this.apiUrl, {
+      params: {
+        api_key: this.apiKey
+      }
+    })
+  }
 }
